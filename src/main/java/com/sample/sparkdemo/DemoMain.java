a25d14a00b72feabdc55c74bb47f24235c3a743b
Penyakamaayo <Penya@penyakamaayo.com>
2017-02-24 17:54:33 +0800

    3rd

--- src/main/java/com/sample/sparkdemo/DemoMain.java
+++ src/main/java/com/sample/sparkdemo/DemoMain.java
@@ -7,20 +7,15 @@ import spark.ModelAndView;
 import spark.template.freemarker.FreeMarkerEngine;
 import static spark.Spark.*;
 
-/**
- *
- * @author jaypax
- */
 public class DemoMain {
 
     public static void main(String[] args) {
         staticFiles.location("/css"); // Static files
         
-        get("/hello", (req, res) -> {
+        get("/index", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
-            model.put("title","SparkJava Demo");
-            model.put("message", "Hello Freemarker!");
-            return new ModelAndView(model, "hello.ftl"); // located in src/test/resources/spark/template/freemarker
+            model.put("title","KDWORLD");
+            return new ModelAndView(model, "index.ftl"); // located in src/test/resources/spark/template/freemarker
         }, new FreeMarkerEngine());
     }
 }
